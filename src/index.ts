52640f1d3bde44c9b2db92afd7380aa5331579f5
jefti <jeftimeira@gmail.com>
2023-09-21 15:56:39 -0300

    feature: error handler function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express  from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
+import router from "./1. routers/index.router";
+import { errorHandler } from "./2. middlewares/errorHandler";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(router);
+app.use(errorHandler);
 
 const port: Number = Number(process.env.Port) || 5000;
 app.listen(port, ()=>{console.log(`Server running on port ${port}...`)});
